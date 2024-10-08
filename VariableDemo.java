c1ffa2dc1f856b6e5a34ff61285deae0bc6bcc01
yashkumar84 <166642691+yashkumar84@users.noreply.github.com>
2024-10-08 17:29:50 +0530

    Add files via upload

---
+++ VariableDemo.java
@@ -0,0 +1,21 @@
+public class VariableDemo {
+  public static void main(String[] args) {
+      int d =  12; //4 Byte 
+      
+      //Datatype
+      // 1. int
+      // 2. float
+      float f = 34.67f; //4 Byte
+      //3. boolean
+      boolean h = true; // 1 Bit
+      short o = 34; // 2 Byte
+      long k = 90; // 8 Byte
+      byte b = 127; //1 Byte
+      double uo = 23.456; //8 Byte
+      char ch = 't'; //2 Byte
+      String name = "Yash"; 
+    
+
+  }
+  
+}
