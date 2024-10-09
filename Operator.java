13e275ce9c5932c4b7ca35a0b761fa4eab8e5772
yashkumar84 <166642691+yashkumar84@users.noreply.github.com>
2024-10-09 16:04:05 +0530

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,28 @@
+public class Operator {
+
+  public static void main(String[] args) {
+    int a =1;
+    int b = 2;
+
+    //Arithmetic Operator  -- +, - , * , / , % 
+
+    // int c = b / a;
+    // int d  = b % a;
+    // System.out.println(d);
+    //Unary Operators
+    // System.out.println(a--);
+    // System.out.println(a);
+
+    //Comparision Operator  == , != , >= , <= , < , > 
+    System.out.println(a >= b);
+
+    //Logical operators 
+    
+
+    System.out.println(a == 1 && b==3);
+    System.out.println(a == 1 || b==3);
+    System.out.println(!(a == 10));
+
+  }
+  
+}
